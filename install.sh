e930488f595bcd7a3b8367bc8294277a6386c41d
Igor Timoshenko <igor.timoshenko@i.ua>
2014-03-24 16:16:20 +0000

    Fixed syntax

--- install.sh
+++ install.sh
@@ -12,7 +12,6 @@ git clone --recursive https://github.com/IgorTimoshenko/dotfiles.git $dotfiles
 cd $dotfiles
 
 echo -n "Would you like to bootstrap new system [Y/n]? "
-
 read item
 case "$item" in
     y|Y) sh $dotfiles/bootstrap-new-system.sh
